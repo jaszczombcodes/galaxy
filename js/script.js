9b0f1440f7f7767f0c77f982884cffcd0d215122
jaszczombcodes <jastrzebska.martyna01@wp.pl>
2023-03-08 17:08:31 +0100

    added: stars different styles, dark spot with stars

--- js/script.js
+++ js/script.js
@@ -2,16 +2,15 @@
 function generateStars(quantity) {
     let star = '<div class="star"></div>'
 
-    let colors = ['red', 'green', 'blue']; 
+   // let colors = ['red', 'green', 'blue']; 
 
     $('.space').each(function(index) {
         for(let i = 0; i < quantity; i++)   {
             $(this).append(star)
         }
 
-        $(this).find('.star').css('background-color', colors[index]); 
+        //$(this).find('.star').css('background-color', colors[index]); 
       });
-
 }
 
 function setStarsPosition() {
@@ -21,23 +20,32 @@ function setStarsPosition() {
     $('.star').each(function() {
         var marginTop = Math.floor(Math.random() * height) 
         var marginLeft = Math.floor(Math.random() * width) 
-        $(this).css({'top': marginTop, 'left': marginLeft})
+        $(this).css({
+            'top': marginTop, 
+            'left': marginLeft
+        })
       });
 
 }
 
+function parallaxEffect () {
+    let value = window.scrollY;
+    let parallaxValues = ['0.5', '0.35', '0.125']; 
+
+    $('.space').each(function(index) {
+        $(this).css("top" , value * parallaxValues[index] + 'px')
+    });
+
+    $('.darkspot').css("top" , value * 0.5 + 'px')
+}
+
  
   $(document).ready(function() {
-    generateStars(200)
+    generateStars(100)
     setStarsPosition()
     
   });
 
   $(window).scroll(function() {
-    let value = window.scrollY;
-    let parallaxValues = ['0.5', '0.25', '0.125']; 
-
-    $('.space').each(function(index) {
-        $(this).css("top" , value * parallaxValues[index] + 'px')
-    });
+    parallaxEffect()
   })
